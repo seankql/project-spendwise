fe82c58e8d051c5c3a2b3c7fd0d412e06f3d3031
Jonathanbai0137 <jonathanbai0317@outlook.com>
2023-03-03 16:40:06 -0500

    added report api

--- backend/controllers/reports_controller.js
+++ backend/controllers/reports_controller.js
@@ -56,3 +56,53 @@ reportsController.get("/", async (req, res) => {
     return res.status(500).send("Internal Server error");
   }
 });
+
+// Get All Transactions for one accounts: GET /api/reports/accounts?accountId=${}&startDate=${}&endDate=${}
+reportsController.get("/accounts", async (req, res) => {
+  try {
+    if (!req.query.accountId || !req.query.startDate || !req.query.endDate) {
+      return res
+        .status(400)
+        .send(
+          "Missing required fields. Must contain [accountId, startDate, endDate]"
+        );
+    }
+    const ids = {
+      accountId: req.query.accountId,
+    };
+    const resultReport =
+      await TransactionModel.getAllTransactionsByIdsAndDateRange(
+        ids,
+        req.query.startDate,
+        req.query.endDate
+      );
+    if (resultReport) {
+      const result = resultReport.reduce(
+        (acc, transaction) => {
+          if (transaction.category && transaction.amount) {
+            const category = transaction.category;
+            const amount = parseInt(transaction.amount);
+            if (IncomeCategories.includes(category)) {
+              acc.income += amount;
+            } else {
+              acc.expense += amount;
+            }
+            return acc;
+          }
+        },
+        { income: 0, expense: 0 }
+      );
+
+      return res.status(200).send({
+        transactions: resultReport,
+        income: result.income,
+        expense: result.expense,
+      });
+    } else {
+      return res.status(400).send("Error getting report");
+    }
+  } catch (err) {
+    Sentry.captureException(err);
+    return res.status(500).send("Internal Server error");
+  }
+});
